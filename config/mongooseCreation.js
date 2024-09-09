54dfc0b90a1b87350f1e03a6e67ed740a5bdacf8
M. Usman Raza <mu951882@gmail.com>
2024-09-09 17:30:53 +0500

    ready to deploy

--- config/mongooseCreation.js
+++ config/mongooseCreation.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-//creating mongoose server and handling errors
+// Connecting to MongoDB Atlas
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  .connect(config.get("MONGODB_URI"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
-    dbgr("connected");
+    dbgr("Connected to MongoDB...");
   })
   .catch((err) => {
-    dbgr(err);
+    dbgr("Error connecting to MongoDB:", err);
   });
 
-//Exporting mongoose.connection means exporting the controll of the db.
+// Exporting mongoose connection for use in other parts of the app
 module.exports = mongoose.connection;
